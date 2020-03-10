c828700f95fb1b89b8f46adc4f7f51031af8e89a
Shivam Shrirao <shivamshrirao@gmail.com>
2020-03-10 10:38:46 +0530

    mongodb connected

--- src/main/java/com/example/loginApp/LoginAppApplication.java
+++ src/main/java/com/example/loginApp/LoginAppApplication.java
@@ -1,13 +1,40 @@
 package com.example.loginApp;
 
+import com.example.loginApp.model.Customer;
+import com.example.loginApp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginAppApplication {
+public class LoginAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAppApplication.class, args);
 	}
 
+	@Autowired
+	private CustomerRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception{
+		repo.deleteAll();
+
+		repo.save(new Customer("Alice","Smith"));
+		repo.save(new Customer("Bob","Smith"));
+
+		System.out.println("Customers:");
+		for (Customer customer : repo.findAll()){
+			System.out.println(customer);
+		}
+		System.out.println("\nby first name\n");
+		System.out.println(repo.findByFirstName("Alice"));
+		System.out.println("by last name\n");
+		System.out.println(repo.findByLastName("Smith"));
+		System.out.println("line by line");
+		for (Customer customer : repo.findByLastName("Smith")){
+			System.out.println(customer);
+		}
+	}
 }
